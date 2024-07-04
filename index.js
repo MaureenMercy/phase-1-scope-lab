c88978cd0a1340549fded33fbf9ab016c47caf5c
MaureenMercy <watetumaureen108@gmail.com>
2024-07-04 08:31:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'some initial value';
+
+// Function that attempts to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = 'attempt to change';
+  } catch (error) {
+    console.log("Can't change a constant!");
+  }
+}
